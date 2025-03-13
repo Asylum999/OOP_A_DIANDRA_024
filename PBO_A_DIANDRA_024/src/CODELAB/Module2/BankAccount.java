f67e11611e000c656b2ffffb8863e5440732bde4
Asylum999 <adzkadiandra@gmail.com>
2025-03-13 14:35:22 +0700

    update-

---
+++ PBO_A_DIANDRA_024/src/CODELAB/Module2/BankAccount.java
@@ -0,0 +1,43 @@
+public class BankAccount {
+    String accountNumber;
+    String ownerName;
+    double balance;
+
+    BankAccount(String aN, String oN, double bL){
+        this.accountNumber = aN;
+        this.ownerName = oN;
+        this.balance = bL;
+    }
+
+    void displayinfo(){
+        System.out.println("Nomor Rekening: " + accountNumber);
+        System.out.println("Nama Pemilik: " + ownerName);
+        System.out.println("Saldo: Rp" + balance);
+        System.out.print("\n");
+    }
+
+    void depositinfo(double ammount){
+        balance += ammount;
+        System.out.println(ownerName + " menyetor Rp" + ammount + ". Saldo sekarang: Rp" + balance);
+    }
+
+    String periksa(double ammount,double balance){
+        if(ammount > balance){
+            return "(GAGAL, Saldo tidak mencukupi)";
+        }
+        return  "(Berhasil)";
+    }
+
+    double tariktunai(double ammount, double balance){
+        if(ammount > balance){
+            return balance;
+        }
+        return balance - ammount;
+    }
+
+    void withdrawMoney(double ammount){
+        balance = tariktunai(ammount, balance);
+        System.out.println(ownerName + " Menarik RP" + ammount + periksa(ammount, balance) + " Saldo sekarang: Rp" +balance);
+
+    }
+}
